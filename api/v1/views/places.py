efcc8c1a61a2b87f38ad6773fab477b127dae2f2
Samuel Lutumba <lutumbasamuel9@gmail.com>
2022-10-25 01:14:48 +0200

    At this point I don't know

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,116 +1,94 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+Place Class from Models Module
 """
-from api.v1.views import app_views
-from flask import abort, jsonify, request
-from flasgger.utils import swag_from
-from models import storage, CNC
-from os import environ
-STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey,\
+    MetaData, Table, ForeignKey
+from sqlalchemy.orm import backref
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+
+if STORAGE_TYPE == "db":
+    class PlaceAmenity(Base):
+        """ PlaceAmenity Class """
+        __tablename__ = 'place_amenity'
+        metadata = Base.metadata
+        place_id = Column(String(60),
+                          ForeignKey('places.id'),
+                          nullable=False,
+                          primary_key=True)
+        amenity_id = Column(String(60),
+                            ForeignKey('amenities.id'),
+                            nullable=False,
+                            primary_key=True)
 
 
-@swag_from('swagger_yaml/places_by_city.yml', methods=['GET', 'POST'])
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
-def places_per_city(city_id=None):
-    """
-        places route to handle http method for requested places by city
-    """
-    city_obj = storage.get('City', city_id)
-    if city_obj is None:
-        abort(404, 'Not found')
+class Place(BaseModel, Base):
+    """Place class handles all application places"""
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
 
-    if request.method == 'GET':
-        all_places = storage.all('Place')
-        city_places = [obj.to_json() for obj in all_places.values()
-                       if obj.city_id == city_id]
-        return jsonify(city_places)
-
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        user_id = req_json.get("user_id")
-        if user_id is None:
-            abort(400, 'Missing user_id')
-        user_obj = storage.get('User', user_id)
-        if user_obj is None:
-            abort(404, 'Not found')
-        if req_json.get("name") is None:
-            abort(400, 'Missing name')
-        Place = CNC.get("Place")
-        req_json['city_id'] = city_id
-        new_object = Place(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
-
-
-@swag_from('swagger_yaml/places_id.yml', methods=['GET', 'DELETE', 'PUT'])
-@app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'])
-def places_with_id(place_id=None):
-    """
-        places route to handle http methods for given place
-    """
-    place_obj = storage.get('Place', place_id)
-    if place_obj is None:
-        abort(404, 'Not found')
-
-    if request.method == 'GET':
-        return jsonify(place_obj.to_json())
-
-    if request.method == 'DELETE':
-        place_obj.delete()
-        del place_obj
-        return jsonify({}), 200
-
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        place_obj.bm_update(req_json)
-        return jsonify(place_obj.to_json()), 200
-
-
-@app_views.route('/places_search', methods=['POST'])
-def places_search():
-    """
-        places route to handle http method for request to search places
-    """
-    all_places = [p for p in storage.all('Place').values()]
-    req_json = request.get_json()
-    if req_json is None:
-        abort(400, 'Not a JSON')
-    states = req_json.get('states')
-    if states and len(states) > 0:
-        all_cities = storage.all('City')
-        state_cities = set([city.id for city in all_cities.values()
-                            if city.state_id in states])
+        amenities = relationship('Amenity', secondary="place_amenity",
+                                 viewonly=False)
+        reviews = relationship('Review', backref='place', cascade='delete')
     else:
-        state_cities = set()
-    cities = req_json.get('cities')
-    if cities and len(cities) > 0:
-        cities = set([
-            c_id for c_id in cities if storage.get('City', c_id)])
-        state_cities = state_cities.union(cities)
-    amenities = req_json.get('amenities')
-    if len(state_cities) > 0:
-        all_places = [p for p in all_places if p.city_id in state_cities]
-    elif amenities is None or len(amenities) == 0:
-        result = [place.to_json() for place in all_places]
-        return jsonify(result)
-    places_amenities = []
-    if amenities and len(amenities) > 0:
-        amenities = set([
-            a_id for a_id in amenities if storage.get('Amenity', a_id)])
-        for p in all_places:
-            p_amenities = None
-            if STORAGE_TYPE == 'db' and p.amenities:
-                p_amenities = [a.id for a in p.amenities]
-            elif len(p.amenities) > 0:
-                p_amenities = p.amenities
-            if p_amenities and all([a in p_amenities for a in amenities]):
-                places_amenities.append(p)
-    else:
-        places_amenities = all_places
-    result = [place.to_json() for place in places_amenities]
-    return jsonify(result)
+        city_id = ''
+        user_id = ''
+        name = ''
+        description = ''
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+        review_ids = []
+
+        @property
+        def amenities(self):
+            """
+                getter for amenitiess list, i.e. amenities attribute of self
+            """
+            if len(self.amenity_ids) > 0:
+                return amenity_ids
+            else:
+                return None
+
+        @amenities.setter
+        def amenities(self, amenity_obj):
+            """
+                setter for amenity_ids
+            """
+            if amenity_obj and amenity_obj not in self.amenity_ids:
+                self.amenity_ids.append(amenity_obj.id)
+
+        @property
+        def reviews(self):
+            """
+                getter for reviews list, i.e. reviews attribute of self
+            """
+            if len(self.review_ids) > 0:
+                return review_ids
+            else:
+                return None
+
+        @reviews.setter
+        def reviews(self, review_obj):
+            """
+                setter for review_ids
+            """
+            if review_obj and review_obj not in self.review_ids:
+                self.review_ids.append(review_obj.id)
